4091be08324930680b1b016a9ab05e61900d879d
SM-Saim <saim777m@example.com>
2023-09-20 20:16:12 +0600

    dynamic loader applied

--- src/Pages/Product/Product.jsx
+++ src/Pages/Product/Product.jsx
@@ -1,10 +1,12 @@
-import { Link } from "react-router-dom";
+import { useLoaderData } from "react-router-dom";
 
-const ProductCard = ({ product }) => {
-  const { id, images, price, title } = product;
+const Product = () => {
+  const product = useLoaderData();
+  console.log(product);
+  const { id, images, price, title, stock, description } = product;
 
   return (
-    <div>
+    <div className="flex h-screen justify-center items-center">
       <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
         <a href="#">
           <img
@@ -69,18 +71,19 @@ const ProductCard = ({ product }) => {
               5.0
             </span>
           </div>
+          <div>Description: {description} </div>
+          <div>Stock: {stock} </div>
           <div className="flex items-center justify-between">
             <span className="text-3xl font-bold text-gray-900 dark:text-white">
               {price}$
             </span>
-            <Link to={`/products/${id}`}>
-              <button
-                href="#"
-                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-              >
-                View Details
-              </button>
-            </Link>
+
+            <button
+              href="#"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Add to Cart
+            </button>
           </div>
         </div>
       </div>
@@ -88,4 +91,4 @@ const ProductCard = ({ product }) => {
   );
 };
 
-export default ProductCard;
+export default Product;
--- src/Route/Route.jsx
+++ src/Route/Route.jsx
@@ -3,6 +3,7 @@ import MainLayOuts from "../layouts/MainLayOuts";
 import Home from "../Pages/Home/Home";
 import Products from "../Pages/Products/Products";
 import DashBoard from "../Pages/DashBoard/DashBoard";
+import Product from "../Pages/Product/Product";
 
 const myCreatedRoute = createBrowserRouter([
   {
@@ -17,6 +18,12 @@ const myCreatedRoute = createBrowserRouter([
         path: "/products",
         element: <Products></Products>,
         loader: () => fetch("https://dummyjson.com/products"),
+      },
+      {
+        path: "/products/:id",
+        element: <Product></Product>,
+        loader: ({ params }) =>
+          fetch(`https://dummyjson.com/products/${params.id}`),
       },
       {
         path: "/dashboard",
